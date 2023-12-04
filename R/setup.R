78e84fb1aefbebc2784a34eff33722982c0d2c7c
Fabio A. Cruz Sanchez <cruzsanc1@univ-lorraine.fr>
2023-12-04 15:20:45 +0100

    adding quarto version

---
+++ R/setup.R
@@ -0,0 +1,20 @@
+# Load packages -----
+library(tidyverse)
+library(here)
+library(countdown)
+
+options(htmltools.dir.version = FALSE)
+knitr::opts_chunk$set(
+  fig.width=9, fig.height=3.5, fig.retina=3,
+  cache = FALSE,
+  echo = FALSE,
+  message = FALSE, 
+  warning = FALSE,
+  hiline = TRUE,
+  fig.align='center',
+  out.width = '75%' 
+)
+
+# Link for the Figures
+URL = c('https://raw.githubusercontent.com/fabbiocrux/Figures/main/')
+
