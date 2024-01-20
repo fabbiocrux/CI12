29cebd38beadcd12455770b3d3566ef9555ce133
Fabio A. Cruz Sanchez <cruzsanc1@univ-lorraine.fr>
2024-01-20 15:51:41 +0100

    avanzando en script

---
+++ 04-open-science/00-Data-Analysis.R
@@ -0,0 +1,11 @@
+
+# ================ Introduction à la Recherche Reproductible ================
+#  But: Faire une script reproductible d'analyse de donnes
+
+# CONTENTS
+#   0. Introduction
+#   1. Reading the Excel/CSV Data
+#   2. Transformer les donnes
+
+
+
